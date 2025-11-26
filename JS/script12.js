6db80d4157ab7ea061114ddbab5ad756f4f9fe75
Lorena <pontongeidy@gmail.com>
2025-11-25 23:17:00 -0500

    se sube contactanos con actualizacion en estilo,script

--- JS/script12.js
+++ JS/script12.js
@@ -42,4 +42,47 @@ botones.forEach(boton => {
     });
 });
 
+// formulario prom
+
+const form = document.getElementById("form-contacto");
+
+form.addEventListener("submit", async (e) => {
+    e.preventDefault(); // Evita recargar la página
+
+    const data = new FormData(form);
+
+    const response = await fetch(form.action, {
+        method: "POST",
+        body: data,
+        headers: { "Accept": "application/json" }
+    });
+
+    if (response.ok) {
+        alert("Tu mensaje ha sido enviado correctamente ❤️");
+        form.reset();
+    } else {
+        alert("Hubo un error al enviar el mensaje.");
+    }
+});
+
+
+// preguntas frecuentes
+const btnFAQ = document.querySelector('.btn-faq-toggle');
+const faqBox = document.querySelector('.faq-box');
+
+btnFAQ.addEventListener('click', () => {
+    faqBox.classList.toggle('oculto');
+});
+
+// Abrir y cerrar cada pregunta
+const faqQuestions = document.querySelectorAll('.faq-question');
+
+faqQuestions.forEach(btn => {
+    btn.addEventListener('click', () => {
+        btn.classList.toggle('active');
+        btn.nextElementSibling.classList.toggle('open');
+    });
+});
+
+
 
