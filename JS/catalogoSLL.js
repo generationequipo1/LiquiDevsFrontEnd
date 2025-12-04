36febaec7337fa5a7bd91a9fa3b0e95c27be8764
luisvillada <otrodeluisvillada@gmail.com>
2025-12-03 23:20:13 -0500

    se modifica la seccion de botones y se empieza la implementacion de funcionalidades

---
+++ JS/catalogoSLL.js
@@ -0,0 +1,8 @@
+const boton_opciones = document.getElementsByClassName("tab-btn");
+
+boton_borrar.addEventListener('click',mostrarOpciones);
+
+function mostrarOpciones(){
+    const mostrar_opciones = document.getElementsByClassName("contenedor-mostrar-opciones");
+     mostrarOpciones
+}
