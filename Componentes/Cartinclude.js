1715b8d86878197226e52def9d1002c91dde39e4
Samary Cardozo <samarycardozo@gmail.com>
2026-01-05 17:00:12 -0500

    Añadir en cartinclude uin if para inicializar el carrito y desplegarlo

--- Componentes/Cartinclude.js
+++ Componentes/Cartinclude.js
@@ -2,4 +2,9 @@ fetch("/Componentes/carrito.html")
   .then(res => res.text())
   .then(html => {
     document.body.insertAdjacentHTML("beforeend", html);
+    
+    //aqui esto para inciialziar el carrito
+    if (typeof initCart === "function") {
+      initCart();
+    }
   });
